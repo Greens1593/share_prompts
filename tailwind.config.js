f2c3be8542b3cf84ae3b09f8a4a44845268e0040
Mike Green <greens1593@gmail.com>
2023-05-29 19:56:28 +0300

    Prompt card component was created.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ module.exports = {
       boxShadow: {
         btn: "inset 10px -50px 94px 0 rgb(199, 199, 199, 0.2)",
       },
+      width: {
+        "max-content": "max-content",
+      },
     },
   },
   plugins: [],
